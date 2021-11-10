5a7da3240e69860d35656267c157cedf75f3fa4f
talesofterror <mich.rivera84@gmail.com>
2021-11-10 13:36:07 -0500

    fucking around at work

--- sketch.js
+++ sketch.js
@@ -13,8 +13,8 @@ function setup() {
 function draw() {
   background(0);
 
-  fill(111, 15, 250);
-  strokeColor1 = color(250);
+  fillColor = color(50, 160, 250);
+  strokeColor1 = color(240, 250, 25);
 
   ticker++;
   time = ticker/tickerMod;
@@ -30,18 +30,27 @@ function draw() {
 
   stroke(strokeColor1);
   strokeWeight(2);
+  fill(fillColor);
+  rect(locX, locY, cosX, sinY);
+
+  stroke(strokeColor1);
+  strokeWeight(2);
+  fill(fillColor);
   line(locX, locY, locX + cosX, locY + sinY);
 
   stroke(strokeColor1);
   strokeWeight(2);
+  fill(fillColor);
   circle(locX, locY, cosX/3);
 
   stroke(strokeColor1);
   strokeWeight(5);
+  fill(fillColor);
   point(locX, locY);
 
   stroke(strokeColor1);
   strokeWeight(2);
+  fill(fillColor);
   circle(locX + cosX, locY + sinY, radius);
   
 }
