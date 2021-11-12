bdc35909ee71c32796407ec8897297e7a0cff6f4
mike <mich.rivera84@gmail.com>
2021-11-11 22:36:17 -0500

    incorporated some css learnings and harmonized the colors (still ugly though)

--- sketch.js
+++ sketch.js
@@ -5,52 +5,80 @@ let time;
 
 
 function setup() {
-  createCanvas(400, 400);
+  // createCanvas(400, 400);
   frameRate(24);
+
+  let renderer = createCanvas(400, 400);
+  renderer.parent("sketch");
 }
 
 
 function draw() {
-  background(0);
+  backgroundColor = (0);
+  
+  background(backgroundColor);
 
-  fillColor = color(50, 160, 250);
-  strokeColor1 = color(240, 250, 25);
+  fillColor = color(238, 210, 83);
+  strokeColor1 = color(255, 0, 64);
 
   ticker++;
   time = ticker/tickerMod;
-
+  
   let locX = width/2;
   let locY = height/2;
-  let radius = 50;
   let angle = 0;
   let amp = 100;
-
+  
   let cosX = amp * cos(angle + time);
   let sinY = amp * sin(angle + time);
-
-  stroke(strokeColor1);
-  strokeWeight(2);
-  fill(fillColor);
-  rect(locX, locY, cosX, sinY);
-
-  stroke(strokeColor1);
-  strokeWeight(2);
-  fill(fillColor);
-  line(locX, locY, locX + cosX, locY + sinY);
-
-  stroke(strokeColor1);
-  strokeWeight(2);
-  fill(fillColor);
-  circle(locX, locY, cosX/3);
-
-  stroke(strokeColor1);
-  strokeWeight(5);
-  fill(fillColor);
-  point(locX, locY);
-
-  stroke(strokeColor1);
-  strokeWeight(2);
-  fill(fillColor);
-  circle(locX + cosX, locY + sinY, radius);
   
+  let outerCircRad = 50;
+  let outerCircCentX = locX + cosX;
+  let outerCircCentY = locY + sinY;
+
+  morphRect();
+
+  Line();
+
+  centerCirc();
+
+  centerDot();
+
+  outerCirc();
+  
+
+  function outerCirc() {
+    stroke(strokeColor1);
+    strokeWeight(2);
+    fill(fillColor);
+    circle(outerCircCentX, outerCircCentY, outerCircRad);
+  }
+
+  function centerDot() {
+    stroke(strokeColor1);
+    strokeWeight(5);
+    fill(fillColor);
+    point(locX, locY);
+  }
+
+  function centerCirc() {
+    stroke(strokeColor1);
+    strokeWeight(2);
+    fill(fillColor);
+    circle(locX, locY, cosX / 3);
+  }
+
+  function morphRect() {
+    stroke(100, 100, 100);
+    strokeWeight(0.2);
+    fill(20, 20, 20);
+    rect(locX, locY, cosX, sinY);
+  }
+
+  function Line() {
+    stroke(strokeColor1);
+    strokeWeight(2);
+    fill(fillColor);
+    line(locX, locY, locX + cosX, locY + sinY);
+  }
 }
