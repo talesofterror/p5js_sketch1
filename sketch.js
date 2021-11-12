12738e5bbddf46761f66ae8655194628d050ac5f
talesofterror <mich.rivera84@gmail.com>
2021-11-12 13:10:02 -0500

    figured out the middle circle

--- sketch.js
+++ sketch.js
@@ -14,44 +14,63 @@ function setup() {
 
 
 function draw() {
-  backgroundColor = (0);
+  backgroundColor = (25, 25, 25);
   
   background(backgroundColor);
 
-  fillColor = color(238, 210, 83);
-  strokeColor1 = color(255, 0, 64);
+  fillColor = color(0, 255, 136);
+  strokeColor1 = color(250, 255, 136);
 
   ticker++;
   time = ticker/tickerMod;
   
-  let locX = width/2;
-  let locY = height/2;
   let angle = 0;
   let amp = 100;
   
   let cosX = amp * cos(angle + time);
   let sinY = amp * sin(angle + time);
   
+  let locX = width/2;
+  let locY = height/2;
   let outerCircRad = 50;
   let outerCircCentX = locX + cosX;
   let outerCircCentY = locY + sinY;
 
+  let midDistX = locX + cosX/2;
+  let midDistY = locY + sinY/2;
+
+  // print("midDistX = " + midDistX);
+  // print("midDistY = " + midDistY);
+  
+  fill(backgroundColor);
+  stroke(strokeColor1);
+  strokeWeight(2);
+  rect(20, 20, width-40, height-40);
+  
   morphRect();
-
+  
   Line();
-
+  
   centerCirc();
-
+  
   centerDot();
-
+  
   outerCirc();
   
+  midCirc();
+
+  function midCirc() {
+    stroke(strokeColor1);
+    strokeWeight(2);
+    fill(fillColor);
+    circle(midDistX, midDistY, cosX/6);
+  }
 
   function outerCirc() {
     stroke(strokeColor1);
     strokeWeight(2);
     fill(fillColor);
-    circle(outerCircCentX, outerCircCentY, outerCircRad);
+    circle(outerCircCentX, outerCircCentY, outerCircRad-sinY/5);
   }
 
   function centerDot() {
@@ -69,9 +88,9 @@ function draw() {
   }
 
   function morphRect() {
-    stroke(100, 100, 100);
-    strokeWeight(0.2);
-    fill(20, 20, 20);
+    stroke(strokeColor1);
+    strokeWeight(2);
+    fill(backgroundColor);
     rect(locX, locY, cosX, sinY);
   }
 
@@ -79,6 +98,6 @@ function draw() {
     stroke(strokeColor1);
     strokeWeight(2);
     fill(fillColor);
-    line(locX, locY, locX + cosX, locY + sinY);
+    line(locX, locY, outerCircCentX, outerCircCentY);
   }
 }
