28fb766f764563a078ea5bd54b09db14a17ece6d
mike <mich.rivera84@gmail.com>
2021-11-12 22:23:52 -0500

    pulled the whole thing out into a global function for no reason maybe. At this point I could randomize the location and size.

--- sketch.js
+++ sketch.js
@@ -23,54 +23,83 @@ function draw() {
 
   ticker++;
   time = ticker/tickerMod;
+
+  backgroundRect();
+  function backgroundRect() {
+    fill(backgroundColor);
+    stroke(strokeColor1);
+    strokeWeight(2);
+    rect(20, 20, width - 40, height - 40);
+  }
   
+  clock1();
+}
+
+
+function clock1() {
+
   let angle = 0;
+  const cosine = cos(angle + time);
+  const sine = sin(angle + time);
+
+  // print("Cosine =" + cosine);
+  // print("Sine =" + sine);
+
   let amp = 100;
-  
-  let cosX = amp * cos(angle + time);
-  let sinY = amp * sin(angle + time);
-  
-  let locX = width/2;
-  let locY = height/2;
+  let cosX = amp * cosine;
+  let sinY = amp * sine;
+
+  let backgroundCircRad = amp * 2;
+
+  // let locX = mouseX;
+  // let locY = mouseY;
+  let locX = width / 2;
+  let locY = height / 2;
   let outerCircRad = 50;
   let outerCircCentX = locX + cosX;
   let outerCircCentY = locY + sinY;
 
-  let midDistX = locX + cosX/2;
-  let midDistY = locY + sinY/2;
+  let midDistX = locX + cosX / 2;
+  let midDistY = locY + sinY / 2;
 
   // print("midDistX = " + midDistX);
   // print("midDistY = " + midDistY);
-  
-  fill(backgroundColor);
-  stroke(strokeColor1);
-  strokeWeight(2);
-  rect(20, 20, width-40, height-40);
-  
+
+
+  backgroundCirc();
+
   morphRect();
-  
+
   Line();
-  
+
   centerCirc();
-  
+
   centerDot();
-  
+
   outerCirc();
-  
+
   midCirc();
+
+
+  function backgroundCirc() {
+    stroke(strokeColor1);
+    strokeWeight(2);
+    fill(backgroundColor);
+    circle(locX, locY, backgroundCircRad);
+  }
 
   function midCirc() {
     stroke(strokeColor1);
     strokeWeight(2);
     fill(fillColor);
-    circle(midDistX, midDistY, cosX/6);
+    circle(midDistX, midDistY, cosX / 6);
   }
 
   function outerCirc() {
     stroke(strokeColor1);
     strokeWeight(2);
     fill(fillColor);
-    circle(outerCircCentX, outerCircCentY, outerCircRad-sinY/5);
+    circle(outerCircCentX, outerCircCentY, outerCircRad - sinY / 5);
   }
 
   function centerDot() {
@@ -101,3 +130,4 @@ function draw() {
     line(locX, locY, outerCircCentX, outerCircCentY);
   }
 }
+
